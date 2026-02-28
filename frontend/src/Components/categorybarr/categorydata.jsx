67a1234ff28d643506a0d969dc0348205719c256
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-02-28 22:07:55 +0530

    feat: added category data file

---
+++ frontend/src/Components/categorybarr/categorydata.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+import fashion from "../../assets/imgicon/image.png";
+import mobiles from "../../assets/imgicon/mobile.webp";
+import travel from "../../assets/imgicon/travel.webp";
+import kitchen from "../../assets/imgicon/kitchen.webp";
+import appliances from "../../assets/imgicon/appliances.webp";
+import beauty from "../../assets/imgicon/beauty.webp";
+import ele from "../../assets/imgicon/ele.webp";
+
+const category=[
+    {name: "fashion",image: fashion,path:'/mens'},
+    {name: "mobile",image: mobiles,path: '/mobiles'},
+    {name: "travel",image: travel,path: '/travel'},
+    {name: "kitchen",image: kitchen,path: '/kitchen'},
+    {name: "appliances",image: appliances,path: '/appliances'},
+    {name: "beauty",image: beauty,path: '/beauty'},
+    {name: "electronic",image: ele,path: '/electronic'},
+
+];
+
+export default category;
