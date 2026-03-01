143db56f6f0a5cfc66f6ccb15d4ad7cc76748cc7
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-03-01 22:54:34 +0530

    feat: updated Login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
+import '../../CSS/Login.css'
 
 const login = () => {
+  
   return (
-    <div>login</div>
+   <div className="contaner">
+    <div className="signup-container">
+      <h1>Login</h1>
+      <div className="input-f">
+        <input type="Email" placeholder='Email'/>
+        <input type="password" placeholder='password' />
+      </div>
+      <button>Login</button>
+
+        <div className="divider">OR</div>
+
+        <button className="google-btn">
+          <img
+            src="https://developers.google.com/identity/images/g-logo.png"
+            alt="google"
+          />
+          Continue with Google
+        </button>
+
+        <button className="facebook-btn">
+          Continue with Facebook
+        </button>
+      <p className="sign-text">
+        Do you not have account ?  <span> <Link to='/Signup'>Sign up</Link> </span>
+      </p>
+    </div>
+   </div>
   )
 }
 
