24fec21643dcb414dddc57aaea7a7f3746486710
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-03-02 22:53:34 +0530

    feat: updated Login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link} from 'react-router-dom'
 import '../../CSS/Login.css'
 
-const login = () => {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async() => {
+    const res = await fetch("http://localhost:5000/api/auth/login", {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await res.json();
+    if(res.ok){
+      localStorage.setItem("token",data.token);
+      localStorage.setItem("name",data.user.name);
+      localStorage.setItem("email",data.user.email);
+      if (data.user.phone) localStorage.setItem("phone", data.user.phone);
+      if (data.user.address) localStorage.setItem("address", data.user.address);
+      window.location.href="/";
+    }else{
+      alert(data.message);
+    }
+  };
   
   return (
    <div className="contaner">
     <div className="signup-container">
       <h1>Login</h1>
       <div className="input-f">
-        <input type="Email" placeholder='Email'/>
-        <input type="password" placeholder='password' />
+        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
-      <button>Login</button>
+      <button onClick={handleLogin}>Login</button>
 
         <div className="divider">OR</div>
 
@@ -35,4 +62,4 @@ const login = () => {
   )
 }
 
-export default login
+export default Login
