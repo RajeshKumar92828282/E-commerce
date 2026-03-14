04f18841404241f7f6bfeec48cfaaffc36532704
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-03-14 21:56:02 +0530

    update

--- frontend/src/pages/categoryitem/Mens.jsx
+++ frontend/src/pages/categoryitem/Mens.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Mens = () => {
   return (
     <div>Mens
-     
+     <div>
+      <div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque et officia praesentium doloribus, sit quis repudiandae, quibusdam molestias, iusto facere laborum excepturi harum voluptate cupiditate ipsam ullam. Quaerat, eius!
+        </div>
+      </div>
+     </div>
     </div>
   )
 }
