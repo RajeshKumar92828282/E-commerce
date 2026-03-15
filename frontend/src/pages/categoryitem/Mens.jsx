19f13d15ac6d30bfe324cf75f6b72182f141e30d
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-03-15 23:09:23 +0530

    update

--- frontend/src/pages/categoryitem/Mens.jsx
+++ frontend/src/pages/categoryitem/Mens.jsx
@@ -9,7 +9,9 @@ const Mens = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque et officia praesentium doloribus, sit quis repudiandae, quibusdam molestias, iusto facere laborum excepturi harum voluptate cupiditate ipsam ullam. Quaerat, eius!
         </div>
       </div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio error blanditiis voluptate soluta iure quod vel placeat, tempora esse, possimus velit ratione voluptates quisquam accusamus dolorum odit recusandae. Animi, cum!
      </div>
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit soluta libero itaque possimus quidem nulla quisquam repudiandae nemo natus sit quas laudantium sunt amet assumenda nisi, expedita pariatur sint? Rem?
     </div>
   )
 }
